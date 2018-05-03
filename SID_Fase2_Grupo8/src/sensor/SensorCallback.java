e409c770ca116f917b58d21d7b6dd403cb20c881
Markiyan <markiyan@iscte-iul.pt>
2018-05-03 18:10:32 +0100

    Sensor working and posting info to db.

---
+++ SID_Fase2_Grupo8/src/sensor/SensorCallback.java
@@ -0,0 +1,36 @@
+package sensor;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+
+public class SensorCallback implements MqttCallback {
+	
+	private DBCollection collection;
+	
+	public SensorCallback( DBCollection collection) {
+		this.collection = collection;
+	}
+	
+	
+	public void connectionLost(Throwable cause) {
+	}
+
+	@Override
+	public void messageArrived(String topic, MqttMessage message) throws Exception {
+		JSONObject jobj = new JSONObject(message.toString());
+		BasicDBObject document = new BasicDBObject();
+		document.put("humidade", jobj.get("humidity"));
+		document.put("temperatura", jobj.get("temperature"));
+		document.put("data", jobj.get("time"));
+		collection.insert(document);
+	}
+
+	public void deliveryComplete(IMqttDeliveryToken token) {
+	}
+
+}
