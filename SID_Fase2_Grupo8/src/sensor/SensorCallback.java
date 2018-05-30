e53ebafc6036596878daab0859e904173c07c2d4
Markiyan <markiyan@iscte-iul.pt>
2018-05-30 17:20:04 +0100

    Fixed datatype/json checking in SensorCallback

--- SID_Fase2_Grupo8/src/sensor/SensorCallback.java
+++ SID_Fase2_Grupo8/src/sensor/SensorCallback.java
@@ -6,10 +6,9 @@ import org.bson.Document;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
 import com.mongodb.client.MongoCollection;
 
 public class SensorCallback implements MqttCallback {
@@ -25,20 +24,30 @@ public class SensorCallback implements MqttCallback {
 	}
 
 	@Override
-	public void messageArrived(String topic, MqttMessage message) throws Exception {
+	public void messageArrived(String topic, MqttMessage message) {
 		System.out.println("Message Arrived!");
-		JSONObject jobj = new JSONObject(message.toString());
-			
+		JSONObject jobj = null;
+		// need to check if it's a JSON object at all
 		try {
-			if(Integer.parseInt(((String) jobj.get("date")).split("/")[2]) == Calendar.YEAR)
-			Double.parseDouble(((String) jobj.get("temperature")));
-			Double.parseDouble(((String) jobj.get("humidity")));
-			collection.insertOne(new Document().append("date", jobj.get("date")).append("time", jobj.get("time"))
-			.append("temperatura", jobj.get("temperature")).append("humidade", jobj.get("humidity")));
-			System.out.println("iok");
-		} catch (Exception e) {
-			System.out.println(e);
+			jobj = new JSONObject(message.toString());
+			//now check if the data is correct
+			try {
+				int year = Integer.parseInt(((String) jobj.get("date")).split("/")[2]);
+				if (year > Calendar.getInstance().get(Calendar.YEAR))
+					throw new IllegalArgumentException("Invalid year!");
+				
+				Double.parseDouble(((String) jobj.get("temperature")));
+				Double.parseDouble(((String) jobj.get("humidity")));
+				collection.insertOne(new Document().append("date", jobj.get("date")).append("time", jobj.get("time"))
+						.append("temperatura", jobj.get("temperature")).append("humidade", jobj.get("humidity")));
+				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		} catch (JSONException e) {
+			System.out.println("Invalid message: Detected non-JSON message in broker?");
 		}
+
 	}
 
 	public void deliveryComplete(IMqttDeliveryToken token) {
